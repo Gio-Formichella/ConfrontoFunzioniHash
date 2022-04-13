719a2da2c2cfc31bf6bad6b3a81aecdf9c5b7da8
Gio <gioformichella@gmail.com>
2022-04-13 18:15:42 +0200

    Project skeleton

---
+++ linked_list.py
@@ -0,0 +1,55 @@
+from node import *
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.get_next()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def print_l(self):
+        current = self.head
+        previous = None
+        while current != None:
+            print("..", current.get_data())
+            current = current.get_next()
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
---
+++ mul hash table.py
@@ -0,0 +1,18 @@
+from linked_list import LinkedList
+
+
+class MulHashTable:
+    def __init__(self, m):
+        self.m = m
+        self.T = []
+        for i in range(0, m):
+            self.T.append(LinkedList())
+
+    def mul_hash_insert(self, key):
+        pass
+
+    def mul_hash_remove(self, key):
+        pass
+
+    def mul_hash_search(self, key):
+        pass
