eb257ea5e640b2d204c7f9614cc97e923ed85342
Gio <gioformichella@gmail.com>
2022-05-14 16:38:00 +0200

    Added insertion results plotting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import numpy as np
+
 import insertion_test
 import search_test
 import remove_test
+import pickle
+import matplotlib.pyplot as plt
 
 test1 = insertion_test
 test2 = search_test
@@ -8,3 +12,10 @@ test3 = remove_test
 exec("test1")
 exec("test2")
 exec("test3")
+
+ins_time = pickle.load(open("results/insertion/averaged_insertion_time.p", "rb"))
+x = list(ins_time.keys())
+y = list(ins_time.values())
+plt.bar(x, y, color='C1', width=0.5)
+plt.ylabel("$\mu$s")
+plt.show()
