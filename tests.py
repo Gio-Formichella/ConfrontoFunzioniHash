7147c3d284dcbc0646aa86bfd7947b045818f36a
Gio <gioformichella@gmail.com>
2022-07-29 10:38:11 +0200

    changed and moved insertion_test.py to test file

--- tests.py
+++ tests.py
@@ -4,14 +4,14 @@ from div_hash_table import *
 from mul_hash_table import *
 import pickle
 
-dimensions = [2048, 2053]
+table_dim = [2048, 2053]
 
 
 def hash_function_test():  # testing average hash function execution time
     div_function_time = 0
     mul_function_time = 0
 
-    for dim in dimensions:
+    for dim in table_dim:
         div_table = DivHashTable(dim)
         mul_table = MulHashTable(dim)
         for i in range(0, 100):
@@ -28,7 +28,35 @@ def hash_function_test():  # testing average hash function execution time
             mul_function_time += end - start
 
     # storing average hash function execution time in micro seconds
-    result = {"DivHashFunction": div_function_time * (10 ** 6) / (len(dimensions) * 100),
-              "MulHashFunction": mul_function_time * (10 ** 6) / (len(dimensions) * 100)}
+    result = {"DivHashFunction": div_function_time * (10 ** 6) / (len(table_dim) * 100),
+              "MulHashFunction": mul_function_time * (10 ** 6) / (len(table_dim) * 100)}
 
     pickle.dump(result, open("results/hash_function/hash_function_result.p", "wb"))
+
+
+def insertion_test():
+    for dim in table_dim:
+        keys = np.random.randint(0, 9999, dim * 2)  # U = [0,9999], max load factor = 2
+
+        mul_table = MulHashTable(dim)
+        div_table = DivHashTable(dim)
+
+        # storing number of collisions every 50 insertions
+        mul_collisions = {}
+        div_collisions = {}
+        mul_col_holder = 0
+        div_col_holder = 0
+        for i in range(0, len(keys)):
+            key = keys[i]
+            mul_col_holder += mul_table.mul_hash_insert(key)
+            div_col_holder += div_table.div_hash_insert(key)
+
+            if (i + 1) % 50 == 0:
+                mul_collisions[i + 1] = mul_col_holder
+                div_collisions[i + 1] = div_col_holder
+                mul_col_holder = 0
+                div_col_holder = 0
+
+        pickle.dump(keys, open("results/insertion/keys_dim=" + str(dim) + ".p", "wb"))
+        pickle.dump(mul_collisions, open("results/insertion/mul_collisions_dim=" + str(dim) + ".p", "wb"))
+        pickle.dump(div_collisions, open("results/insertion/div_collisions_dim=" + str(dim) + ".p", "wb"))
