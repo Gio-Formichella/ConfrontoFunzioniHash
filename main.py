1e124e86bbb924f1935ecb4b6ec94bf44baacbe1
Gio <gioformichella@gmail.com>
2022-07-29 11:36:07 +0200

    search test plots

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tests
 
 # exec("tests.hash_function_test()")
 # exec("tests.insertion_test()")
+# exec("tests.search_test()")
 
 # hash_function_test plot
 result = pickle.load(open("results/hash_function/hash_function_result.p", "rb"))
@@ -36,5 +37,26 @@ plt.figure()
 plt.plot(x1, y1, 'r', label="DivHashTable")
 plt.plot(x2, y2, 'orange', label="MulHashTable")
 plt.ylabel("collisions")
+
+# search plot
+div_inspected = pickle.load(open("results/search/div_inspected_dim=2048.p", "rb"))
+mul_inspected = pickle.load(open("results/search/mul_inspected_dim=2048.p", "rb"))
+x1 = list(div_inspected.keys())
+y1 = list(div_inspected.values())
+x2 = list(mul_inspected.keys())
+y2 = list(mul_inspected.values())
+plt.figure()
+plt.plot(x1, y1, 'r', label="DivHashTable")
+plt.plot(x2, y2, 'orange', label="MulHashTable")
+
+div_inspected = pickle.load(open("results/search/div_inspected_dim=2053.p", "rb"))
+mul_inspected = pickle.load(open("results/search/mul_inspected_dim=2053.p", "rb"))
+x1 = list(div_inspected.keys())
+y1 = list(div_inspected.values())
+x2 = list(mul_inspected.keys())
+y2 = list(mul_inspected.values())
+plt.figure()
+plt.plot(x1, y1, 'r', label="DivHashTable")
+plt.plot(x2, y2, 'orange', label="MulHashTable")
 
 plt.show()
